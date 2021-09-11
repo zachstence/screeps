8a20a240bd39bff8b66d29d550ee940757e86a35
Zachary Stence <zachstence@gmail.com>
2021-09-11 13:10:34 -0500

    feat: trim git branch name in deploy

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,9 @@ import {execSync} from "child_process";
 
 import config from "./config.json";
 
-const branchName = execSync("git rev-parse --abbrev-ref HEAD").toString();
+const branchName = execSync("git rev-parse --abbrev-ref HEAD")
+    .toString()
+    .trim();
 if (!branchName) throw new Error("Could not determine current git branch name");
 
 export default {
